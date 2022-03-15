1c06f0d65e504e68307bcb19f6cb9560aaa3ab1b
tylerstern0 <tylerstern0@gmail.com>
2022-03-15 11:51:01 -0600

    RAVEN run-docker arguments

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-docker run --rm -it aswin1906/raven:1.0 bash raven_framework ./plugins/SR2ML/tests/test_ETmodel.xml
+
+if [ "$1" != "" ]; then
+    OURFILE="$1"
+    echo "Running RAVEN for $OURFILE..."
+else
+    echo "No xml filename provided. Running test_FTmodel.xml..."
+    OURFILE=test_FTmodel.xml
+fi
+
+docker run --rm -it aswin1906/raven:1.0 bash raven_framework ./plugins/SR2ML/tests/$OURFILE
