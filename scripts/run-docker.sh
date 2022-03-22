5fe6bac39e70ce720046552ed42ef33d5d789d4f
tylerstern0 <tylerstern0@gmail.com>
2022-03-22 09:59:19 -0500

    Mount working dir
    Docker script argument for mounting working directory of raven run... this is automatically done when calling this script from the sr2ml backend

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -8,4 +8,12 @@ else
     OURFILE=test_FTmodel.xml
 fi
 
-docker run --rm -it aswin1906/raven:1.0 bash raven_framework ./plugins/SR2ML/tests/$OURFILE
+if [ "$2" != "" ]; then
+    WORKINGDIR="$2"
+    echo "Mounting directory $WORKINGDIR..."
+else
+    echo "No directory provided. Using FTmodel..."
+    WORKINGDIR=FTmodel
+fi
+
+docker run --rm -it -v ../$WORKINGDIR/:/$WORKINGDIR aswin1906/raven:1.0 bash raven_framework ./plugins/SR2ML/tests/$OURFILE
